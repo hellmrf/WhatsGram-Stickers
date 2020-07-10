90b0ddd9b332c338cecbcef804144396dda592a9
Heliton Martins <helitonmrf@gmail.com>
2020-07-10 01:26:35 -0300

    Adding a fresh chromeprofile

--- whatsgramstickers/WhatsappBot.py
+++ whatsgramstickers/WhatsappBot.py
@@ -32,7 +32,7 @@ class WhatsappBot:
             chrome_options=[
                 "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
             ],
-            heroku=True
+            heroku=self._headless
         )
         self._bot_actions = BotActions(self._driver)
 
